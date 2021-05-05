13ace342359606882ac864610ed5b3707a505d21
Ashutosh Vyas <ashutoshavyas@gmail.com>
2021-05-06 04:33:44 +0530

    Add loss and accuracy plotting function.

--- Handwriting_Object_Recognition_4/object_detection_cifar10.py
+++ Handwriting_Object_Recognition_4/object_detection_cifar10.py
@@ -7,8 +7,14 @@ A simple program to build CNN classifier to classify 10 objects from CIFAR10 dat
 """
 import cv2
 import numpy
+import matplotlib.pyplot as plt
 from keras.models import load_model
 from keras.datasets import cifar10
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.optimizers import RMSprop
 
 numpy.random.seed(seed=42)
 
@@ -75,7 +81,7 @@ def display_test_result(test_image, pred_class, window_title, scale, img_width, 
     return 0
 
 
-def image_classifier_cifar10(classifier, img_width=32, img_height=32, img_depth=3, scale=8, color=True):
+def pretrained_saved_classifier(classifier, x_train, y_train, x_test, y_test, img_width=32, img_height=32, img_depth=3, scale=8, color=True):
     """
     CIFAR10 pretrained classifier.
 
@@ -101,8 +107,6 @@ def image_classifier_cifar10(classifier, img_width=32, img_height=32, img_depth=
 
     """
 
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
     for i in range(0, 10):
         random_test_image = x_test[numpy.random.randint(0, len(x_test))]
         test_image_enlarged = cv2.resize(random_test_image, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
@@ -119,6 +123,95 @@ def image_classifier_cifar10(classifier, img_width=32, img_height=32, img_depth=
     return 0
 
 
+def train_cifar10_classifier(x_train, y_train, x_test, y_test):
+
+    # Training Parameters
+    batch_size = 4
+    num_classes = 10
+    epochs = 5
+
+    # change datatype from uint8 to float32 and normalize
+    x_train = x_train.astype('float32')/255
+    x_test = x_test.astype('float32')/255
+
+    # one hot encode target classes
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+    print("Number of Classes: " + str(y_test.shape[1]))
+
+    classifier = Sequential()
+
+    classifier.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+    classifier.add(Activation('relu'))
+    classifier.add(Conv2D(32, (3, 3)))
+    classifier.add(Activation('relu'))
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+    classifier.add(Dropout(0.25))
+
+    classifier.add(Conv2D(64, (3, 3), padding='same'))
+    classifier.add(Activation('relu'))
+    classifier.add(Conv2D(64, (3, 3)))
+    classifier.add(Activation('relu'))
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+    classifier.add(Dropout(0.25))
+
+    classifier.add(Flatten())
+    classifier.add(Dense(512))
+    classifier.add(Activation('relu'))
+    classifier.add(Dropout(0.5))
+    classifier.add(Dense(num_classes))
+    classifier.add(Activation('softmax'))
+
+    # initiate RMSprop optimizer and configure some parameters
+    rmsprop_optimizer = RMSprop(lr=0.0001, decay=1e-6)
+    classifier.compile(loss='categorical_crossentropy', optimizer=rmsprop_optimizer, metrics=['accuracy'])
+
+    print(classifier.summary())
+
+    history = classifier.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
+                             validation_data=(x_test, y_test), shuffle=True)
+
+    classifier.save("./trained_model/cifar10_exercise_cnn.h5")
+
+    # Evaluate the performance of our trained model
+    scores = classifier.evaluate(x_test, y_test, verbose=1)
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
+
+    history_dict = history.history
+
+    plotting_loss_accuracy(history_dict, metric_train="loss", metric_test="val_loss", title="Loss CIFAR10",
+                           label_train="Train loss", label_test="Test loss", x_label="Epochs", y_label="Loss")
+
+    plotting_loss_accuracy(history_dict, metric_train="accuracy", metric_test="val_accuracy", title="Accuracy CIFAR10",
+                           label_train="Train acc.", label_test="Test acc.", x_label="Epochs", y_label="Accuracy")
+
+    return 0
+
+
+def plotting_loss_accuracy(history_dict, metric_train, metric_test, title, label_train, label_test, x_label, y_label):
+
+    metric_train_values = history_dict[metric_train]
+    metric_test_values = history_dict[metric_test]
+    epochs = range(1, len(metric_train_values) + 1)
+
+    line1 = plt.plot(epochs, metric_train_values, label=label_train)
+    line2 = plt.plot(epochs, metric_test_values, label=label_test)
+    plt.setp(line1, linewidth=2.0, marker='+', markersize=10.0)
+    plt.setp(line2, linewidth=2.0, marker='4', markersize=10.0)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    return 0
+
+
 def main():
     """
     The main function to execute upon call.
@@ -131,8 +224,13 @@ def main():
 
     print("Simple classifier for object detection CIFAR10 dataset.")
 
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
     classifier = load_model('./trained_model/cifar_simple_cnn.h5')
-    image_classifier_cifar10(classifier)
+
+    pretrained_saved_classifier(classifier, x_train, y_train, x_test, y_test)
+
+    train_cifar10_classifier(x_train, y_train, x_test, y_test)
 
     print("\nDone")
 
